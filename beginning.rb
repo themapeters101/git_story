72696af70e7ce2241873245e7b9fa05992af417a
Thema Peters <tpete6957@gmail.com>
2018-08-19 16:41:31 -0500

    first version

---
+++ beginning.rb
@@ -0,0 +1,4 @@
+This is a the beginning of the story.
+Once upon a time, in a far away land, there was a girl learning to code.
+She took her time and slowly learned.
+And she eventually learned to ask the right question.
